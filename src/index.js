9bd2cbb95fbf19f11dbb94ff6d5c98aa5ce76925
Maxim-Komogortsev <helgurskoggur@gmail.com>
2022-01-25 23:30:42 +1000

    noUislider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import './styles/index.scss';
 import $ from "jquery";
+import * as noUiSlider from 'nouislider';
+import 'nouislider/dist/nouislider.css';
 
+import './styles/index.scss';
 
+document.querySelectorAll('.rating__star').forEach(item => {
+    item.addEventListener('click', () => {
+        item.parentNode.dataset.totalValue = item.dataset.itemValue
+    })
+     
+})
 
-console.log("it works");
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 5000,
+        'max': 10000
+    }
+});
