68fc7efff80547e44fdca718f87a52fb3d1f7170
Maxim-Komogortsev <helgurskoggur@gmail.com>
2022-03-22 16:28:47 +1000

    dropdown slide feature

--- src/index.js
+++ src/index.js
@@ -3,22 +3,58 @@ import * as noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
 import './styles/index.scss';
 
+
+//Star rating
 document.querySelectorAll('.rating__star').forEach(item => {
     item.addEventListener('click', () => {
         item.parentNode.dataset.totalValue = item.dataset.itemValue
     })
      
-})
-
-var slider = document.getElementById('slider');
-
-noUiSlider.create(slider, {
-    start: [20, 80],
-    connect: true,
-    range: {
-        'min': 5000,
-        'max': 10000
-    }
 });
+
+
+
+//Pagination
+window.onload = function(){
+      //Range slider
+    var slider = document.getElementById('slider');
+
+    noUiSlider.create(slider, {
+        start: [20, 80],
+        connect: true,
+        range: {
+            'min': 5000,
+            'max': 10000
+        }
+    });
+
+    var paginationPage = parseInt($('.cdp').attr('actpage'), 10);
+    $('.cdp_i').on('click', function(){
+      var go = $(this).attr('href').replace('#!', '');
+      if (go === '+1') {
+        paginationPage++;
+      } else if (go === '-1') {
+        paginationPage--;
+      }else{
+        paginationPage = parseInt(go, 10);
+      }
+      $('.cdp').attr('actpage', paginationPage);
+    });
+    //Dropdown-menu-open
+    var flag = 0;
+    var dropdown = $('.dropdown-full');
+    dropdown.click(function () {
+      if (flag == 0){
+        $('.dropdown-menu').slideDown()
+        dropdown.css('border-color', 'rgba(31, 32, 65, 0.5)')
+        flag = 1
+      } else {
+        $('.dropdown-menu').slideUp()
+        dropdown.css('border-color', '')
+        flag = 0
+      }
+    })
+}
+
 
 
